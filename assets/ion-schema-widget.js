f063fbd773651fc6aa2df6fd5b51d2534c118ede
Khushboo Desai <desaikd@amazon.com>
2023-05-03 11:37:28 -0700

    adds helper function to generate violations table

--- assets/ion-schema-widget.js
+++ assets/ion-schema-widget.js
@@ -60,6 +60,44 @@ function _set_output_style(resultDiv, styleName) {
     resultDiv.classList.add(`bs-callout-${styleName}`)
 }
 
+function _generate_violations_table(data, container) {
+    // Create the table element
+    let table = document.createElement("table");
+
+    // Get the keys (column names) of the first object in the given data
+    let cols = Object.keys(data[0]);
+
+    // Create the header element
+    let thead = document.createElement("thead");
+    let tr = document.createElement("tr");
+
+    // Loop through the column names and create header cells
+    cols.forEach((item) => {
+        let th = document.createElement("th");
+        th.innerText = item; // Set the column name as the text of the header cell
+        tr.appendChild(th); // Append the header cell to the header row
+    });
+    thead.appendChild(tr); // Append the header row to the header
+    table.append(tr) // Append the header to the table
+
+    // Loop through the given data and create table rows
+    data.forEach((item) => {
+        let tr = document.createElement("tr");
+
+        // Get the values of the current object in the given data
+        let vals = Object.values(item);
+
+        // Loop through the values and create table cells
+        vals.forEach((elem) => {
+            let td = document.createElement("td");
+            td.innerText = elem; // Set the value as the text of the table cell
+            tr.appendChild(td); // Append the table cell to the table row
+        });
+        table.appendChild(tr); // Append the table row to the table
+    });
+    container.appendChild(table) // Append the table to the container element
+}
+
 const show = () => {
     const schemaContent = ace.edit("schema").getValue();
     const valueContent = ace.edit("value").getValue();
@@ -85,42 +123,7 @@ const show = () => {
                 } else {
                     _set_output_style(resultDiv, "warning")
                     pre.textContent = `${result.value()} is invalid!`;
-
-                    // Create the table element
-                    let table = document.createElement("table");
-
-                    // Get the keys (column names) of the first object in the JSON data
-                    let cols = Object.keys(result.violations()[0]);
-
-                    // Create the header element
-                    let thead = document.createElement("thead");
-                    let tr = document.createElement("tr");
-
-                    // Loop through the column names and create header cells
-                    cols.forEach((item) => {
-                        let th = document.createElement("th");
-                        th.innerText = item; // Set the column name as the text of the header cell
-                        tr.appendChild(th); // Append the header cell to the header row
-                    });
-                    thead.appendChild(tr); // Append the header row to the header
-                    table.append(tr) // Append the header to the table
-
-                    // Loop through the JSON data and create table rows
-                    result.violations().forEach((item) => {
-                        let tr = document.createElement("tr");
-
-                        // Get the values of the current object in the JSON data
-                        let vals = Object.values(item);
-
-                        // Loop through the values and create table cells
-                        vals.forEach((elem) => {
-                            let td = document.createElement("td");
-                            td.innerText = elem; // Set the value as the text of the table cell
-                            tr.appendChild(td); // Append the table cell to the table row
-                        });
-                        table.appendChild(tr); // Append the table row to the table
-                    });
-                    container.appendChild(table) // Append the table to the container element
+                    _generate_violations_table(result.violations(), container)
                 }
             }
 
